df84d0477fb94473e6130e4f49aae444b525ef5a
DL <ge45roh@mytum.de>
2024-12-02 17:58:18 +0100

    solutions day1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         Day1 day1 = new Day1(null);
-        day1.solvePartOne();
+        day1.solvePartTwo();
         day1.solvePartTwo();
     }
 }

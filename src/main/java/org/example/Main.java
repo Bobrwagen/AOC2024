46f4d705edeb90d27663f7a2a28ce90357567400
DL <ge45roh@mytum.de>
2024-12-02 17:54:19 +0100

    solutions day1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Day1 day1 = new Day1(null);
+        day1.solvePartOne();
+        day1.solvePartTwo();
+    }
+}
